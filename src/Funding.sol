27197f33501bf354dd33a780b11f7a48007c7683
milosdjurica <milosdjurica13@gmail.com>
2024-05-08 16:51:10 +0200

    copied contracts

--- src/Funding.sol
+++ src/Funding.sol
@@ -4,15 +4,9 @@ pragma solidity ^0.8.20;
 // ! Uncomment this line to use console.log
 // import "hardhat/console.sol";
 
-////////////////////
-// * Imports 	  //
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
-////////////////////
 
 contract Funding is Ownable {
-    ////////////////////
-    // * Errors 	  //
-    ////////////////////
     error Funding__ZeroAddress();
     error Funding__AmountIsZero();
     error Funding__NotEnoughBalance();
@@ -35,26 +29,8 @@ contract Funding is Ownable {
     // * Modifiers 	  //
     ////////////////////
 
-    ////////////////////
-    // * Functions	  //
-    ////////////////////
-
-    ////////////////////
-    // * Constructor  //
-    ////////////////////
     constructor() Ownable(msg.sender) {}
 
-    ////////////////////////////
-    // * Receive & Fallback   //
-    ////////////////////////////
-
-    ////////////////////
-    // * External 	  //
-    ////////////////////
-
-    ////////////////////
-    // * Public 	  //
-    ////////////////////
     function fund(address _userToFund, uint256 _amount) public onlyOwner {
         if (_userToFund == address(0)) revert Funding__ZeroAddress();
         if (_amount <= 0) revert Funding__AmountIsZero();
@@ -64,16 +40,4 @@ contract Funding is Ownable {
         (bool success,) = _userToFund.call{value: _amount}("");
         if (!success) revert Funding__TransferFailed();
     }
-
-    ////////////////////
-    // * Internal 	  //
-    ////////////////////
-
-    ////////////////////
-    // * Private 	  //
-    ////////////////////
-
-    ////////////////////
-    // * View & Pure  //
-    ////////////////////
 }
