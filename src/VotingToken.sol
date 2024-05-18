6410973bddb54022cb0f7122dc067033653a2a07
milosdjurica <milosdjurica13@gmail.com>
2024-05-18 18:13:06 +0200

    changing to ownable

--- src/VotingToken.sol
+++ src/VotingToken.sol
@@ -5,14 +5,15 @@ import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {ERC20Permit} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {ERC20Votes} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 import {Nonces} from "@openzeppelin/contracts/utils/Nonces.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract VotingToken is ERC20, ERC20Permit, ERC20Votes {
-    constructor() ERC20("VotingToken", "VTK") ERC20Permit("VotingToken") {}
+contract VotingToken is ERC20, ERC20Permit, ERC20Votes, Ownable {
+    constructor() ERC20("VotingToken", "VTK") ERC20Permit("VotingToken") Ownable(msg.sender) {}
 
     // The functions below are overrides required by Solidity.
 
     // TODO -> Can put TimeLock to own this contract and then people from DAO can vote on who can mint voting tokens
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
