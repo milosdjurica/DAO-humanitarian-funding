7dc4f4a88eeca6a361b417eed827d91e566d57c0
milosdjurica <milosdjurica13@gmail.com>
2024-05-25 19:35:37 +0200

    leaving deploy script as it is because of access controls

--- script/DeployAndSetUpContracts.sol
+++ script/DeployAndSetUpContracts.sol
@@ -8,7 +8,6 @@ import {MyGovernor} from "../src/MyGovernor.sol";
 import {Funding} from "../src/Funding.sol";
 import {Constants} from "./Constants.sol";
 import {HelperConfig} from "./HelperConfig.s.sol";
-import {DeployVotingTokenScript} from "./01-deployVotingToken.s.sol";
 
 contract DeployAndSetUpContracts is Script, Constants {
     Funding funding;
@@ -26,11 +25,10 @@ contract DeployAndSetUpContracts is Script, Constants {
         (uint256 interval, address vrfCoordinator, bytes32 gasLane, uint64 subscriptionId, uint32 callbackGasLimit) =
             helperConfig.activeNetworkConfig();
 
-        DeployVotingTokenScript deployVotingTokenContractScript = new DeployVotingTokenScript();
-        votingToken = deployVotingTokenContractScript.run();
+        vm.startBroadcast();
+        votingToken = new VotingToken();
         firstVoter = votingToken.owner();
 
-        vm.startBroadcast();
         timeLock = new TimeLock(MIN_DELAY, proposers, executors);
         myGovernor = new MyGovernor(
             GOVERNOR_NAME, VOTING_DELAY, VOTING_PERIOD, PROPOSAL_THRESHOLD, votingToken, QUORUM_PERCENTAGE, timeLock
